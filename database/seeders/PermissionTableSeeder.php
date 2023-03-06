aba16c5e151e76f7c4615c5dfa7483b519f58d36
LAPTOP-GQT1FNKP\NZR <nizar.adian.n@gmail.com>
2023-03-06 20:40:09 +0700

    - add seeder permission

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -18,81 +18,53 @@ class PermissionTableSeeder extends Seeder
         $permissions = [
 
             // admin panel permissions
-
-            [
-                'name' => 'admin_panel_access',
-            ],
-
+            ['name' => 'admin_panel_access',],
             // user permissions
-
-            [
-                'name' => 'users_access',
-            ],
-
-            [
-                'name' => 'user_edit',
-            ],
-
-            [
-                'name' => 'user_delete',
-            ],
-
-            [
-                'name' => 'user_create',
-            ],
-
-            [
-                'name' => 'user_show',
-            ],
-
-
+            ['name' => 'users_access',],
+            ['name' => 'user_edit',],
+            ['name' => 'user_delete',],
+            ['name' => 'user_create',],
+            ['name' => 'user_show',],
             // role permissions
-
-            [
-                'name' => 'roles_access',
-            ],
-
-            [
-                'name' => 'role_edit',
-            ],
-
-            [
-                'name' => 'role_delete',
-            ],
-
-            [
-                'name' => 'role_create',
-            ],
-
-            [
-                'name' => 'role_show',
-            ],
-
-
+            ['name' => 'roles_access',],
+            ['name' => 'role_edit',],
+            ['name' => 'role_delete',],
+            ['name' => 'role_create',],
+            ['name' => 'role_show',],
             // permission permissions
-
-            [
-                'name' => 'permissions_access',
-            ],
-
-            [
-                'name' => 'permission_edit',
-            ],
-
-            [
-                'name' => 'permission_delete',
-            ],
-
-            [
-                'name' => 'permission_create',
-            ],
-
-
-
+            ['name' => 'permissions_access',],
+            ['name' => 'permission_edit',],
+            ['name' => 'permission_delete',],
+            ['name' => 'permission_create',],
+            // product type permissions
+            ['name' => 'product_type_access',],
+            ['name' => 'product_type_edit',],
+            ['name' => 'product_type_delete',],
+            ['name' => 'product_type_create',],
+            // product permissions
+            ['name' => 'product_access',],
+            ['name' => 'product_edit',],
+            ['name' => 'product_delete',],
+            ['name' => 'product_create',],
+            // customer permissions
+            ['name' => 'customer_access',],
+            ['name' => 'customer_edit',],
+            ['name' => 'customer_delete',],
+            ['name' => 'customer_create',],
+            // invoice permissions
+            ['name' => 'invoice_access',],
+            ['name' => 'invoice_edit',],
+            ['name' => 'invoice_delete',],
+            ['name' => 'invoice_create',],
+            // payment permissions
+            ['name' => 'payment_access',],
+            ['name' => 'payment_edit',],
+            ['name' => 'payment_delete',],
+            ['name' => 'payment_create',],
 
         ];
 
-        foreach($permissions as $permission){
+        foreach ($permissions as $permission) {
             Permission::create($permission);
         }
 
